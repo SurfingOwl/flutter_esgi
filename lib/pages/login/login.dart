a3b95f94e746c94990a5319bd2eb9f9a714b2710
saziz <shan.azizh@gmail.com>
2024-01-29 20:37:04 +0100

    feat: login page ui

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -9,28 +9,19 @@ class Login extends StatelessWidget {
         body: Center(
             child: Column(children: [
               Expanded(
-                  flex: 2,
-                  child: SafeArea(
-                    child: Container(
-                      margin: const EdgeInsets.symmetric(vertical: 1),
-                      child: const Image(
-                          image: AssetImage('assets/flower_logo.png'),
-                          width: 200,
-                          height: 200
-                      ),
+                flex: 2,
+                child: SafeArea(
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(vertical: 2),
+                    child: const Image(
+                        image: AssetImage('assets/flower_logo.png'),
+                        width: 200,
+                        height: 200
                     ),
                   ),
+                )
               ),
-              Expanded(
-                  flex: 1,
-                  child: Text("",
-                      style: Theme.of(context)
-                          .textTheme
-                          .headlineLarge!
-                          .copyWith(color: Colors.white)
-                  )
-              ),
-              const Expanded(flex: 6, child: LoginForm()),
+              const Expanded(flex: 3, child: LoginForm()),
       ],
     )));
   }
@@ -45,44 +36,57 @@ class LoginForm extends StatelessWidget {
       child: Column(
         children: [
           Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical:10),
+              padding: const EdgeInsets.symmetric(vertical: 1),
+              child: Text("GESTWIT", // TODO Font
+                  style: Theme.of(context)
+                      .textTheme
+                      .headlineSmall!
+                      .copyWith(color: Colors.white)
+              )
+          ),
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical:15),
               child: TextFormField(
                 decoration: InputDecoration(
                   labelText: "Adresse email",
                   labelStyle: Theme.of(context)
                         .textTheme
-                        .labelMedium!
+                        .labelLarge!
                         .copyWith(color: Colors.white)
                 ),
               ),
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical:10),
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical:10),
             child: TextFormField(
               decoration: InputDecoration(
                   labelText: "Mot de passe",
                   labelStyle: Theme.of(context)
                       .textTheme
-                      .labelMedium!
+                      .labelLarge!
                       .copyWith(color: Colors.white)
               ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical:10),
-            child: ElevatedButton(
+          TextButton(onPressed: () {  }, child: const Text("Vous n'avez pas de compte ? Inscrivez vous"),),
+          ElevatedButton(
               onPressed: () {
 
               },
+              // TODO as THEME
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)
+                )
+              ),
               child: Text(
                 "Se connecter",
                 style: Theme.of(context)
                     .textTheme
-                    .bodySmall!
+                    .labelLarge!
                     .copyWith(color: Colors.white) ,
               ),
             ),
-          )
         ],
       ),
     );
